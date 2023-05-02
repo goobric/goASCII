0ff7d3d255b920daba08eb83d9ac55dcde0dc04a
mlaane <goobric@gmail.com>
2023-05-03 04:25:04 +0800

    concatonated seperate strings into one string

--- mainf/main.go
+++ mainf/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	inputString := strings.Join(os.Args[1:], " ")
+	fmt.Println(inputString)
+	sepWords := strings.Split(inputString, `\n`)
+	fmt.Println(sepWords)
 }

f8498638fb623e73ce3a21847d28dc563590b98e
mlaane <goobric@gmail.com>
2023-05-03 04:42:50 +0800

    count lines, and count words in string length

--- mainf/main.go
+++ mainf/main.go
@@ -13,10 +13,20 @@ func main() {
 	// seperate the characters to a new line when \n is encountered
 	sepWords := strings.Split(concatString, `\n`)
 	fmt.Println(sepWords)
-
+	// error handling
 	file, err := os.ReadFile("standard.txt")
 	if err != nil {
 		fmt.Println("Error reading file")
 		panic(err)
+	}
+	// count number of lines in each string length
+	lines := strings.Split(string(file), "\n")
+	// count number of words in each line
+	for i, word := range words {
+		if word == "" {
+			if i < len(words)-1 {
+				fmt.Println()
+		}
+		continue
 	}
 }
